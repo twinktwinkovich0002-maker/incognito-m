930fbe5bdae0b31148a1f9764d0f1606a1c09d71
twinktwinkovich0002-maker <twinktwinkovich0002@gmail.com>
2025-11-03 16:31:46 +0300

    Delete incognito-m directory

--- incognito-m/packages/frontend/components/ChatWindow.tsx
+++
@@ -1,7 +0,0 @@
-export default function ChatWindow({ messages }:{ messages:any[] }) {
-  return (
-    <div>
-      {messages.map(m => <div key={m.id}><b>{m.sender_id}</b>: {m.body}</div>)}
-    </div>
-  );
-}
--- incognito-m/packages/frontend/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { domains: ['your-s3-domain.com'] }
-}
-module.exports = nextConfig;
